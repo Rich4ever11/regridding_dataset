703da22586af531d29c158d13611b1b0b737ec9c
Rich4ever11 <rich4ever11@protonmail.com>
2024-11-26 17:06:34 -0500

    added maps/figures for pop dens.

--- density_script.py
+++ density_script.py
@@ -7,6 +7,8 @@ from rasterio.transform import from_origin
 import xarray
 import rasterio
 import rioxarray as riox
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 import sys
 from utilityGlobal import INIT_YEAR
 from utilityFunc import (
@@ -14,7 +16,9 @@ from utilityFunc import (
     obtain_netcdf_files,
     evaluate_upscale_sum,
     resample_matrix,
+    time_series_plot,
     save_file,
+    draw_map,
 )
 
 
@@ -47,6 +51,9 @@ class GeoDataResizePopulationDensity:
         :param: None
         :return: None
         """
+        # _, time_analysis_axis = plt.subplots(figsize=(10, 6))
+        upscale_sum_values = []
+        original_sum_values = []
         for file in self.files:
             try:
                 with Dataset(file) as netcdf_dataset:
@@ -57,6 +64,7 @@ class GeoDataResizePopulationDensity:
                     var_data_array = netcdf_dataset.variables["total-population"][:]
                     time_data_array = netcdf_dataset.variables["time"][:]
 
+                    original_var_data = []
                     upscaled_var_data = []
                     for year in range(len(var_data_array)):
                         curr_year_data_array = var_data_array[year]
@@ -82,21 +90,34 @@ class GeoDataResizePopulationDensity:
                             upscaled_curr_year_data_array, 0
                         )
 
+                        curr_year_data_array = np.flip(curr_year_data_array, 0)
+
                         # adding the empty values back (this is not required )
                         # upscaled_curr_year_data_array[
                         #     upscaled_curr_year_data_array == 0
                         # ] = np.nan
 
+                        upscale_sum_values.append(upscaled_curr_year_data_array.sum())
+                        original_sum_values.append(curr_year_data_array.sum())
+
                         # create the xarray data array for the upscaled burned area and add it to the dictionary
                         upscaled_var_data.append(
                             upscaled_curr_year_data_array,
                         )
+                        original_var_data.append(curr_year_data_array)
 
                     attribute_dict = {}
 
                     # Copy attributes of the burned area fraction
                     for attr_name in var_data.ncattrs():
                         attribute_dict[attr_name] = getattr(var_data, attr_name)
+
+                    map_figure_upscale, map_axis_upscale = plt.subplots(
+                        nrows=1,
+                        ncols=1,
+                        figsize=(18, 10),
+                        subplot_kw={"projection": ccrs.PlateCarree()},
+                    )
 
                     # obtain the height and width from the upscale shape
                     # create an evenly spaced array representing the longitude and the latitude
@@ -114,6 +135,58 @@ class GeoDataResizePopulationDensity:
                         dims=["time", "latitude", "longitude"],
                         attrs=attribute_dict,
                     )
+
+                    draw_map(
+                        map_figure=map_figure_upscale,
+                        map_axis=map_axis_upscale,
+                        units=attribute_dict["units"],
+                        label="Upscaled Population Density Data",
+                        latitude=latitudes,
+                        longitude=longitudes,
+                        var_data_xarray=xarray_data_array,
+                        cbarmac=None,
+                    )
+
+                    # obtain the height and width from the original shape
+                    # create an evenly spaced array representing the longitude and the latitude
+
+                    map_figure_original, map_axis_original = plt.subplots(
+                        nrows=1,
+                        ncols=1,
+                        figsize=(18, 10),
+                        subplot_kw={"projection": ccrs.PlateCarree()},
+                    )
+
+                    longitudes = np.linspace(
+                        -180, 180, np.asarray(original_var_data).shape[-1]
+                    )
+                    latitudes = np.linspace(
+                        -90, 90, np.asarray(original_var_data).shape[-2]
+                    )
+
+                    # creates the data array
+                    original_xarray_data_array = xarray.DataArray(
+                        np.asarray(original_var_data),
+                        coords={
+                            "time": time_data_array,
+                            "latitude": latitudes,
+                            "longitude": longitudes,
+                        },
+                        dims=["time", "latitude", "longitude"],
+                        attrs=attribute_dict,
+                    )
+
+                    draw_map(
+                        map_figure=map_figure_original,
+                        map_axis=map_axis_original,
+                        units=attribute_dict["units"],
+                        label="Original Population Density Data",
+                        latitude=latitudes,
+                        longitude=longitudes,
+                        var_data_xarray=original_xarray_data_array,
+                        cbarmac=None,
+                    )
+
                     dataset_dict["total-population"] = xarray_data_array
                     # saves xarray dataset to a file
                     save_file(
@@ -125,6 +198,75 @@ class GeoDataResizePopulationDensity:
             except Exception as error:
                 print("[-] Failed to parse dataset: ", error)
                 print(traceback.format_exc())
+
+        # data_yearly_upscale = np.column_stack(
+        #     (
+        #         np.arange(
+        #             0,
+        #             len(upscale_sum_values),
+        #         ),
+        #         upscale_sum_values,
+        #     )
+        # )
+
+        # time_series_plot(
+        #     axis=time_analysis_axis,
+        #     data=(data_yearly_upscale),
+        #     marker="o",
+        #     line_style="-",
+        #     color="b",
+        #     label="Population Density Upscaled Data",
+        #     axis_title="Population Data",
+        #     axis_xlabel="1850 - 2014",
+        #     axis_ylabel="Population Density m^2",
+        # )
+
+        # data_yearly_origin = np.column_stack(
+        #     (
+        #         np.arange(
+        #             0,
+        #             len(original_sum_values),
+        #         ),
+        #         original_sum_values,
+        #     )
+        # )
+
+        # time_series_plot(
+        #     axis=time_analysis_axis,
+        #     data=(data_yearly_origin),
+        #     marker="x",
+        #     line_style="-",
+        #     color="r",
+        #     label="Population Density Original Data",
+        #     axis_title="Population Original",
+        #     axis_xlabel="1850 - 2014",
+        #     axis_ylabel=f"Burned Area {attribute_dict['units']}",
+        # )
+
+        # diff_value = np.array(original_sum_values) - np.array(upscale_sum_values)
+        # data_yearly_diff = np.column_stack(
+        #     (
+        #         np.arange(
+        #             0,
+        #             len(diff_value),
+        #         ),
+        #         diff_value,
+        #     )
+        # )
+
+        # time_series_plot(
+        #     axis=time_analysis_axis,
+        #     data=(data_yearly_diff),
+        #     marker="x",
+        #     line_style="-",
+        #     color="r",
+        #     label="GFED5 Burned Area Difference",
+        #     axis_title="GFED5 Burned Area Total Original Data - GFED5 Burned Area Total Upscaled Data",
+        #     axis_xlabel="Monthly 1997 - 2016",
+        #     axis_ylabel="Burned Area m^2",
+        # )
+
+        plt.show()
 
 
 def main():
